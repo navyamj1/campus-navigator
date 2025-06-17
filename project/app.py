6f568bd7263b6b974bf97aa2ed9dd828fa8ca26e
navyamj1 <151832040+navyamj1@users.noreply.github.com>
2025-06-17 18:22:51 +0530

    Update app.py

--- project/app.py
+++ project/app.py
@@ -8,7 +8,8 @@ db_config = {
     "host":os.environ["DB_HOST"],
     "user": os.environ["DB_USER"],
     "password": os.environ["DB_PASSWORD"],
-    "database": os.environ["DB_NAME"]
+    "database": os.environ["DB_NAME"],
+    "port":int(os.environ.get("DB_PORT", 3306))
 }
 
 
